c0b2748ebbae5c3a1c932f0031c23959be8eed69
Didac Oliveira <didac@brainupdaters.net>
2017-01-23 18:28:03 +0100

    Update rear_suse.sh

--- fosdem17_ReaR_DRLM_workshop/shell/rear_suse.sh
+++ fosdem17_ReaR_DRLM_workshop/shell/rear_suse.sh
@@ -6,3 +6,6 @@ useradd -d /home/fosdem17 -m -G sudo fosdem17
 passwd -d -u fosdem17
 chage -d0 fosdem17
 echo "fosdem17 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
+
+
+echo "End of $HOSTNAME customization ..."
