934c07feb18ee81f0edb6b1932c5396ae9f78b39
Didac Oliveira <didac@brainupdaters.net>
2017-01-24 11:28:56 +0100

    Update rear_centos.sh

--- fosdem17_ReaR_DRLM_workshop/shell/rear_centos.sh
+++ fosdem17_ReaR_DRLM_workshop/shell/rear_centos.sh
@@ -3,4 +3,8 @@ echo "Hello from $HOSTNAME at FOSDEM 2017"
 echo "Restarting network service ..."
 systemctl restart network
 
+echo "$(date) - Refreshing repositories ..."
+yum clean all
+yum repolist
+
 echo "End of $HOSTNAME customization ..."
