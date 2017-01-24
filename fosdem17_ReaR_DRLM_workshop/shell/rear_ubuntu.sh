a61a7875db866f3edf5bc857426e172d453bf511
Didac Oliveira <didac@brainupdaters.net>
2017-01-24 23:49:27 +0100

    Update rear_ubuntu.sh

--- fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
+++ fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
@@ -5,4 +5,7 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install kbd
 
+echo "$(date) - Rebooting system to apply changes ..."
+nohup reboot > /dev/null 2>&1 &
+
 echo "End of $HOSTNAME customization ..."
