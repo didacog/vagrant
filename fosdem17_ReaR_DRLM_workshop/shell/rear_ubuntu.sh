8a022a23ebc2b5004b9055963a221274fb14d60f
Didac Oliveira <didac@brainupdaters.net>
2017-01-24 23:47:28 +0100

    Update rear_ubuntu.sh

--- fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
+++ fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
@@ -2,6 +2,7 @@ echo "Hello from $HOSTNAME at FOSDEM 2017"
 
 echo "$(date) - Refreshing repositories ..."
 apt-get update
-apt-get install kbd
+apt-get -y upgrade
+apt-get -y install kbd
 
 echo "End of $HOSTNAME customization ..."
