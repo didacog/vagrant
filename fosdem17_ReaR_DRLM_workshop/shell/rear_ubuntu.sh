ab4649f59cf2b031ff9504ee12a5aa8922295b36
Didac Oliveira <didac@brainupdaters.net>
2017-01-25 00:01:44 +0100

    Update rear_ubuntu.sh

--- fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
+++ fosdem17_ReaR_DRLM_workshop/shell/rear_ubuntu.sh
@@ -3,8 +3,9 @@ echo "Hello from $HOSTNAME at FOSDEM 2017"
 echo "$(date) - Refreshing repositories ..."
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy upgrade
 apt-get -y install kbd
+apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy upgrade
+
 
 echo "$(date) - Rebooting system to apply changes ..."
 nohup reboot > /dev/null 2>&1 &
